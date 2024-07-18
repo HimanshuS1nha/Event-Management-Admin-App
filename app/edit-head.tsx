b18086be2c9e12e510de3a62e56bf09da5b66ae2
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-18 20:00:11 +0530

    New page edit-head added

---
+++ app/edit-head.tsx
@@ -0,0 +1,28 @@
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { useMutation } from "@tanstack/react-query";
+
+import SafeView from "@/components/SafeView";
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+import LoadingModal from "@/components/LoadingModal";
+
+const EditHead = () => {
+  const { mutate: handleEditHead, isPending } = useMutation({
+    mutationKey: ["edit-head"],
+  });
+  return (
+    <SafeView>
+      <LoadingModal isVisible={isPending} />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header showBackButton />
+
+        <Title>Edit Head</Title>
+      </ScrollView>
+    </SafeView>
+  );
+};
+
+export default EditHead;
