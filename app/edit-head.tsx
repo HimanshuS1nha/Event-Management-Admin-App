d1a21caf430109a4d2391b16a6ad92b57c74fe50
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-19 15:57:10 +0530

    edit-head page UI complete

--- app/edit-head.tsx
+++ app/edit-head.tsx
@@ -1,8 +1,19 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Pressable,
+  TextInput,
+  Image,
+  Alert,
+} from "react-native";
+import React, { useCallback, useState } from "react";
 import tw from "twrnc";
 import { useMutation } from "@tanstack/react-query";
 import { useLocalSearchParams, router } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+import * as ImagePicker from "expo-image-picker";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import SafeView from "@/components/SafeView";
 import Header from "@/components/Header";
@@ -12,6 +23,41 @@ import LoadingModal from "@/components/LoadingModal";
 const EditHead = () => {
   const { id } = useLocalSearchParams();
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [image, setImage] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleChange = useCallback(
+    (type: "name" | "email" | "phoneNumber", value: string) => {
+      if (type === "name") {
+        setName(value);
+      } else if (type === "email") {
+        setEmail(value);
+      } else if (type === "phoneNumber") {
+        setPhoneNumber(value);
+      }
+    },
+    []
+  );
+
+  const pickImage = useCallback(async () => {
+    try {
+      const res = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        base64: true,
+      });
+      if (res.canceled) {
+        return;
+      }
+      const base64 = `data:image/png;base64,${res.assets?.[0].base64}`;
+      setImage(base64);
+    } catch (error) {
+      Alert.alert("Error", "Some error occured.");
+    }
+  }, []);
+
   const { mutate: handleEditHead, isPending } = useMutation({
     mutationKey: ["edit-head"],
   });
@@ -19,10 +65,78 @@ const EditHead = () => {
     <SafeView>
       <LoadingModal isVisible={isPending} />
 
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={tw`pb-8`}
+      >
         <Header showBackButton />
 
         <Title>Edit Head</Title>
+
+        <View style={tw`gap-y-6 w-full items-center mt-10`}>
+          {image && (
+            <View style={tw`items-center`}>
+              <View>
+                <Image
+                  source={{
+                    uri: image,
+                  }}
+                  style={tw`w-32 h-32 rounded-full`}
+                />
+                <Pressable
+                  style={tw`absolute bottom-0 right-0`}
+                  onPress={pickImage}
+                >
+                  <MaterialIcons name="change-circle" size={40} color="white" />
+                </Pressable>
+              </View>
+            </View>
+          )}
+          <View style={tw`gap-y-3 w-[80%]`}>
+            <Text style={tw`text-white ml-1.5 font-medium text-base`}>
+              Name
+            </Text>
+            <TextInput
+              style={tw`w-full border border-white px-4 py-3 rounded-lg text-white`}
+              placeholder="Enter your name"
+              placeholderTextColor={"#fff"}
+              value={name}
+              onChangeText={(text) => handleChange("name", text)}
+            />
+          </View>
+          <View style={tw`gap-y-3 w-[80%]`}>
+            <Text style={tw`text-white ml-1.5 font-medium text-base`}>
+              Email
+            </Text>
+            <TextInput
+              style={tw`w-full border border-white px-4 py-3 rounded-lg text-white`}
+              placeholder="Enter your email"
+              placeholderTextColor={"#fff"}
+              value={email}
+              onChangeText={(text) => handleChange("email", text)}
+            />
+          </View>
+          <View style={tw`gap-y-3 w-[80%]`}>
+            <Text style={tw`text-white ml-1.5 font-medium text-base`}>
+              Phone Number
+            </Text>
+            <TextInput
+              style={tw`w-full border border-white px-4 py-3 rounded-lg text-white`}
+              placeholder="Enter your phone number"
+              placeholderTextColor={"#fff"}
+              value={phoneNumber}
+              onChangeText={(text) => handleChange("phoneNumber", text)}
+              keyboardType="number-pad"
+            />
+          </View>
+          <Pressable
+            style={tw`bg-violet-600 w-[80%] items-center py-3 justify-center rounded-lg`}
+            onPress={() => handleEditHead()}
+            disabled={isPending}
+          >
+            <Text style={tw`text-white text-base font-semibold`}>Add</Text>
+          </Pressable>
+        </View>
       </ScrollView>
     </SafeView>
   );
--- components/ScannerCard.tsx
+++ components/ScannerCard.tsx
@@ -1,8 +1,7 @@
 import { View, Text, Pressable, Alert } from "react-native";
 import React, { memo } from "react";
 import tw from "twrnc";
-import { Feather, FontAwesome } from "@expo/vector-icons";
-import { router } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
 
 const ScannerCard = ({
   scanner,
@@ -18,35 +17,22 @@ const ScannerCard = ({
       <Text style={tw`text-white font-medium text-base w-[70%]`}>
         {scanner.email}
       </Text>
-      <View style={tw`flex-row gap-x-4 items-center`}>
-        <Pressable
-          style={tw`bg-green-600 p-2 rounded-full items-center justify-center`}
-          onPress={() =>
-            router.push({
-              pathname: "/edit-scanner",
-              params: { scannerEmail: scanner.email },
-            })
-          }
-        >
-          <Feather name="edit-2" size={20} color="white" />
-        </Pressable>
-        <Pressable
-          style={tw`bg-rose-600 p-2 rounded-full items-center justify-center`}
-          onPress={() => {
-            Alert.alert("Warning", "Do you want to delete this event?", [
-              {
-                text: "No",
-              },
-              {
-                text: "Yes",
-                onPress: () => onDelete(scanner.id),
-              },
-            ]);
-          }}
-        >
-          <FontAwesome name="trash" size={20} color="white" />
-        </Pressable>
-      </View>
+      <Pressable
+        style={tw`bg-rose-600 p-2 rounded-full items-center justify-center`}
+        onPress={() => {
+          Alert.alert("Warning", "Do you want to delete this event?", [
+            {
+              text: "No",
+            },
+            {
+              text: "Yes",
+              onPress: () => onDelete(scanner.id),
+            },
+          ]);
+        }}
+      >
+        <FontAwesome name="trash" size={20} color="white" />
+      </Pressable>
     </View>
   );
 };
