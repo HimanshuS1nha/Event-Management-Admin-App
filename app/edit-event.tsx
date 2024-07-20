84ccd26c3c9b31320463da1e8aa082b2409d9da1
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-20 19:51:52 +0530

    New page edit-event added

---
+++ app/edit-event.tsx
@@ -0,0 +1,25 @@
+import { View, Text } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { useMutation } from "@tanstack/react-query";
+
+import SafeView from "@/components/SafeView";
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+import LoadingModal from "@/components/LoadingModal";
+
+const EditEvent = () => {
+  const { mutate: handleEditEvent, isPending } = useMutation({
+    mutationKey: ["edit-event"],
+  });
+  return (
+    <SafeView>
+      <LoadingModal isVisible={isPending} />
+      <Header showBackButton />
+
+      <Title>Add Event</Title>
+    </SafeView>
+  );
+};
+
+export default EditEvent;
