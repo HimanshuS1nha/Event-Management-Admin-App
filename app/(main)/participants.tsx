e8f7ea496cebc6190ee388b23b014ef5dc4e9886
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-23 14:33:27 +0530

    participants page UI complete

--- app/(main)/participants.tsx
+++ app/(main)/participants.tsx
@@ -1,24 +1,51 @@
-import {
-  View,
-  Text,
-  Pressable,
-  ImageBackground,
-  ScrollView,
-} from "react-native";
+import { View, Text, Pressable, ScrollView } from "react-native";
 import React from "react";
 import tw from "twrnc";
+import { FlashList } from "@shopify/flash-list";
 
 import SafeView from "@/components/SafeView";
 import Title from "@/components/Title";
 import Header from "@/components/Header";
+import LoadingModal from "@/components/LoadingModal";
+import ParticipantCard from "@/components/ParticipantCard";
+import { useMutation } from "@tanstack/react-query";
 
 const Participants = () => {
+  const participants = [
+    {
+      id: "1",
+      name: "Random Guy",
+      branch: "CSE",
+      year: "3rd",
+      image:
+        "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ];
+
+  const { mutate: handleDelete, isPending } = useMutation({
+    mutationKey: ["delete-user"],
+    mutationFn: async (id: string) => {},
+  });
   return (
     <SafeView>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Header />
 
         <Title>Participants</Title>
+
+        <View style={tw`mt-8 w-full h-full px-4`}>
+          <FlashList
+            data={participants}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+              return (
+                <ParticipantCard participant={item} onDelete={handleDelete} />
+              );
+            }}
+            estimatedItemSize={40}
+            numColumns={2}
+          />
+        </View>
       </ScrollView>
     </SafeView>
   );
--- components/ParticipantCard.tsx
+++ components/ParticipantCard.tsx
@@ -1,20 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, Pressable, Alert } from "react-native";
 import React, { memo } from "react";
+import tw from "twrnc";
+import { FontAwesome } from "@expo/vector-icons";
 
 const ParticipantCard = ({
   participant,
+  onDelete,
 }: {
   participant: {
     id: string;
     image: string;
-    namae: string;
+    name: string;
     branch: string;
     year: string;
   };
+  onDelete: (id: string) => void;
 }) => {
   return (
-    <View>
-      <Text>ParticipantCard</Text>
+    <View
+      style={tw`bg-gray-700 rounded-lg py-4 w-[95%] mb-2.5 items-center justify-center gap-y-3.5`}
+    >
+      <Image
+        source={{ uri: participant.image }}
+        style={tw`w-24 h-24 rounded-full`}
+      />
+
+      <View style={tw`gap-y-1 items-center`}>
+        <Text style={tw`text-xl font-medium text-white px-2 text-center`}>
+          {participant.name}
+        </Text>
+        <Text style={tw`font-medium text-gray-300`}>
+          {participant.branch} {participant.year} year
+        </Text>
+      </View>
+
+      <Pressable
+        style={tw`bg-rose-600 p-1.5 rounded-full items-center justify-center absolute top-1.5 right-1.5`}
+        onPress={() => {
+          Alert.alert("Warning", "Do you want to delete this event?", [
+            {
+              text: "No",
+            },
+            {
+              text: "Yes",
+              onPress: () => onDelete(participant.id),
+            },
+          ]);
+        }}
+      >
+        <FontAwesome name="trash" size={20} color="white" />
+      </Pressable>
     </View>
   );
 };
